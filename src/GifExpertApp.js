419541fdadc5140841c8bb0a8d0e35f6cefce725
Carles Vicedo <carlesvicedo@gmail.com>
2021-07-04 13:11:05 +0200

    Add gif-expert-app

---
+++ src/GifExpertApp.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory'
+import { GifGrid } from './components/GifGrid'
+
+const GifExpertApp = () => {
+
+const [categories, setCategories] = useState(['The Simpsons'])
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory 
+                setCategories={ setCategories }
+            />
+            <hr />
+
+            <ol>
+                {
+                categories.map(category => 
+                <GifGrid 
+                    category={category}
+                    key={category}
+                />
+                )
+                }
+            </ol>
+        </>
+    )
+}
+
+export default GifExpertApp;
---
+++ src/components/GifGrid.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
+import { GifGridItem } from './GifGridItem'
+
+
+
+export const GifGrid = ({category}) => {
+    debugger
+
+    const {data: images, loading} = useFetchGifs( category )
+
+    const imagesYes = (images) => {
+        return(
+            <div className="card-grid">
+                    {images.map((img) => {
+                        return(
+                            <GifGridItem
+                                key={img.id}
+                                {...img}
+                            />
+                        )
+                    })}
+            </div>
+        )
+    }
+
+    const imagesNo = () => <p>Sorry, no gifs found.</p>;
+        
+    return (
+        <>
+            <h3 className="animate__animated animate__fadeIn">{category}</h3>
+
+            {loading && <p className="animate__animated animate__flash">Loading...</p>}
+            
+            {images.length === 0 ? imagesNo() : imagesYes(images) }
+            
+        </>
+    )
+}

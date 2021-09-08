133cba9113a1e213513c7295a4eac390083d06c8
Carles Vicedo <carlesvicedo@gmail.com>
2021-09-08 19:26:09 +0200

    Add GifGrid component tests

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useFetchGifs } from '../hooks/useFetchGifs'
 import { GifGridItem } from './GifGridItem'
+import PropTypes from 'prop-types'
 
 
 
@@ -35,4 +36,8 @@ export const GifGrid = ({category}) => {
             
         </>
     )
+}
+
+GifGrid.propTypes = {
+    category: PropTypes.string.isRequired
 }
